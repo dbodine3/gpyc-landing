190ce19f5f52993a841c874469c11c0c059ea227
Eric Malcolm <eric@malcolmdigital.com>
2017-12-28 15:07:46 -0500

    All added styles and templates for landing page and the website base.

---
+++ base-template-full.php
@@ -0,0 +1,28 @@
+<?php
+
+use Roots\Sage\Setup;
+use Roots\Sage\Wrapper;
+
+?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+  <?php get_template_part('templates/head'); ?>
+  <body <?php body_class(); ?>>
+    <!--[if IE]>
+      <div class="alert alert-warning">
+        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
+      </div>
+    <![endif]-->
+    <div class="wrap" role="document">
+      <div class="content row">
+        <main class="main">
+          <?php include Wrapper\template_path(); ?>
+        </main><!-- /.main -->
+      </div><!-- /.content -->
+    </div><!-- /.wrap -->
+    <?php
+      wp_footer();
+    ?>
+  </body>
+</html>
---
+++ template-full.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template Name: Full Width Template
+ */
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php the_content(); ?>
+<?php endwhile; ?>
