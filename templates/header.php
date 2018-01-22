1f42e66bf1e72157e79e7763b158f6ec58309215
Eric Malcolm <eric@malcolmdigital.com>
2018-01-22 17:12:21 -0500

    more style, separating out and clean up, main nav redo, etc.

--- templates/header.php
+++ templates/header.php
@@ -1,70 +1,30 @@
-<header class="banner">
-    <!--<div class="container">
-        <div class="row">
-            <div class="col"></div>
-            <div class="col col-4">
-                <a class="brand" href="<?= esc_url(home_url('/')); ?>">
-                    <img src="https://place-hold.it/300x70" class="img-fluid" alt="<?php bloginfo('name'); ?>">
-                </a>
-            </div>
-            <div class="col"></div>
+<header>
+    <div class="gpyc-header container-fluid">
+        <div class="gpyc-header__logo-container">
+            <a class="gpyc-header__logo" href="<?= esc_url(home_url('/')); ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/dist/images/gpyc_logo.jpg" class="img-fluid" alt="<?php bloginfo('name'); ?>">
+            </a>
         </div>
-        <nav>
-            <div class="row">
-                <div class="col col-4">
-                    <ul class="row">
-                        <li class="col">About</li>
-                        <li class="col">Harbor</li>
-                        <li class="col">Activities</li>
-                    </ul>
-                </div>
-                <div class="col col-4">
-
-                </div>
-                <div class="col col-4">
-                    <ul class="row">
-                        <li class="col">Events</li>
-                        <li class="col">Members</li>
-                        <li class="col">
-                            <a href="#" class="gpyc-btn gpyc-btn--small gpyc-btn--gold-outline"><span>Membership</span></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+        <nav class="gpyc-header__nav">
+            <ul class="row">
+                <li class="col">
+                    <a href="#" class="gpyc-header__nav__item">About</a>
+                </li>
+                <li class="col">
+                    <a href="#" class="gpyc-header__nav__item">Harbor</a>
+                </li>
+                <li class="col">
+                    <a href="#" class="gpyc-header__nav__item">Activities</a>
+                </li>
+                <li class="col">
+                    <a href="#" class="gpyc-header__nav__item">Events</a>
+                </li>
+                <li class="col">
+                    <a href="#" class="gpyc-btn gpyc-btn--full gpyc-btn--small gpyc-btn--gold-black"><span>Membership</span></a>
+                </li>
+            </ul>
         </nav>
-    </div>-->
-
-        <div class="container">
-            <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <a class="navbar-brand" href="#">Navbar</a>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav navbar-left">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                    </ul>
-                    <ul class="navbar-nav navbar-right">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-
-        </div>
-
-
-
+    </div>
         <!--<nav class="nav-primary">
             <?php
             if (has_nav_menu('primary_navigation')) :
